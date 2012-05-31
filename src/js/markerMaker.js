dbc0716135929786dc5941a7a93adabec82232d4
Homan Chou <homanchou@gmail.com>
2012-05-31 16:13:18 -0700

    basic working markerMaker

---
+++ src/js/markerMaker.js
@@ -0,0 +1,39 @@
+/*
+create a semi-transparent image to be used as a brush tip
+*/
+function markerMaker(length) {
+    this.length = length || 30;
+    this.canvas = $('<canvas width="'+length+'px" height="'+length+'px" style="border:1px solid gray;">');
+    this.context = this.canvas[0].getContext('2d');
+    this.make();
+}
+
+markerMaker.prototype.make = function () {
+   this.context.fillStyle = "rgba(255,0,0,0.5)";
+    this.context.beginPath();
+
+  this.context.moveTo(0,0);
+  this.context.quadraticCurveTo(this.length * 0.6, this.length * 0.0, this.length, this.length);
+  this.context.moveTo(0,0);
+  this.context.quadraticCurveTo(this.length * 0.0, this.length * 0.6, this.length, this.length);
+  this.context.closePath();
+  var grd = this.context.createLinearGradient(0, 0, this.length, this.length);
+        // light blue
+        grd.addColorStop(1, "#8ED6FF");
+        // dark blue
+        grd.addColorStop(0, "rgba(0,255,0,0.05)");
+        this.context.fillStyle = grd;
+  
+  this.context.fill();
+
+
+
+        
+
+
+}
+
+markerMaker.prototype.getImage = function() {
+}
+
+
