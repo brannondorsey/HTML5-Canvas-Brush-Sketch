afcd91c6783fe23fedf54996d9d0aac93f6266c4
Homan Chou <homanchou@gmail.com>
2012-05-30 17:25:12 -0700

    rename sketcher to sketchPad, bind click event to clear button unobtrusively

--- src/js/sketchPad.js
+++ src/js/sketchPad.js
@@ -1,4 +1,4 @@
-function Sketcher( canvasID, brushImage ) {
+function SketchPad( canvasID, brushImage ) {
 	this.renderFunction = (brushImage == null || brushImage == undefined) ? this.updateCanvasByLine : this.updateCanvasByBrush;
 	this.brush = brushImage;
 	this.touchSupported = ('ontouchstart' in window); 
@@ -6,7 +6,8 @@ function Sketcher( canvasID, brushImage ) {
 	this.canvas = $("#"+canvasID);
 	this.context = this.canvas.get(0).getContext("2d");	
 	this.context.strokeStyle = "#000000";
-	this.context.lineWidth = 3;
+  //  this.context.globalCompositeOperation = 'destination-out';
+	this.context.lineWidth = 10;
 	this.lastMousePoint = {x:0, y:0};
     
 	if (this.touchSupported) {
@@ -23,7 +24,7 @@ function Sketcher( canvasID, brushImage ) {
 	this.canvas.bind( this.mouseDownEvent, this.onCanvasMouseDown() );
 }
 
-Sketcher.prototype.onCanvasMouseDown = function () {
+SketchPad.prototype.onCanvasMouseDown = function () {
 	var self = this;
 	return function(event) {
 		self.mouseMoveHandler = self.onCanvasMouseMove()
@@ -37,7 +38,7 @@ Sketcher.prototype.onCanvasMouseDown = function () {
 	}
 }
 
-Sketcher.prototype.onCanvasMouseMove = function () {
+SketchPad.prototype.onCanvasMouseMove = function () {
 	var self = this;
 	return function(event) {
 
@@ -46,7 +47,7 @@ Sketcher.prototype.onCanvasMouseMove = function () {
     	return false;
 	}
 }
-Sketcher.prototype.onCanvasMouseUp = function (event) {
+SketchPad.prototype.onCanvasMouseUp = function (event) {
 	var self = this;
 	return function(event) {
 
@@ -58,7 +59,7 @@ Sketcher.prototype.onCanvasMouseUp = function (event) {
 	}
 }
 
-Sketcher.prototype.updateMousePosition = function (event) {
+SketchPad.prototype.updateMousePosition = function (event) {
  	var target;
 	if (this.touchSupported) {
 		target = event.originalEvent.touches[0]
@@ -73,7 +74,7 @@ Sketcher.prototype.updateMousePosition = function (event) {
 
 }
 
-Sketcher.prototype.updateCanvasByLine = function (event) {
+SketchPad.prototype.updateCanvasByLine = function (event) {
 
 	this.context.beginPath();
 	this.context.moveTo( this.lastMousePoint.x, this.lastMousePoint.y );
@@ -82,14 +83,14 @@ Sketcher.prototype.updateCanvasByLine = function (event) {
 	this.context.stroke();
 }
 
-Sketcher.prototype.updateCanvasByBrush = function (event) {
+SketchPad.prototype.updateCanvasByBrush = function (event) {
 	var start = { x:this.lastMousePoint.x, y: this.lastMousePoint.y };
 	this.updateMousePosition( event );
 	var end = { x:this.lastMousePoint.x, y: this.lastMousePoint.y };
 	this.drawLine( start, end );
 }
 
-Sketcher.prototype.drawLine = function (start, end){
+SketchPad.prototype.drawLine = function (start, end){
 	var halfBrushW = this.brush.width/2;
 	var halfBrushH = this.brush.height/2;
     var dx = end.x - start.x;
@@ -108,7 +109,7 @@ Sketcher.prototype.drawLine = function (start, end){
 	}
 }
 
-Sketcher.prototype.toString = function () {
+SketchPad.prototype.toString = function () {
 
 	var dataString = this.canvas.get(0).toDataURL("image/png");
 	var index = dataString.indexOf( "," )+1;
@@ -117,13 +118,13 @@ Sketcher.prototype.toString = function () {
 	return dataString;
 }
 
-Sketcher.prototype.toDataURL = function () {
+SketchPad.prototype.toDataURL = function () {
 
 	var dataString = this.canvas.get(0).toDataURL("image/png");
 	return dataString;
 }
 
-Sketcher.prototype.clear = function () {
+SketchPad.prototype.clear = function () {
 
 	var c = this.canvas[0];
 	this.context.clearRect( 0, 0, c.width, c.height );
