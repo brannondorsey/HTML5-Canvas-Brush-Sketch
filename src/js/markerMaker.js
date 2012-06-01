15a145784dd970004d0cade0bed4b44b337b3676
Homan Chou <homanchou@gmail.com>
2012-06-01 16:55:43 -0700

    working destination-in marker

--- src/js/markerMaker.js
+++ src/js/markerMaker.js
@@ -1,12 +1,42 @@
 /*
 create a semi-transparent image to be used as a brush tip
 */
-function markerMaker(length,color) {
+function markerMaker(stamp_img,color) {
+   this.length = stamp_img.width;
+   this.canvas = $('<canvas width="'+this.length+'px" height="'+this.length+'px">');
+   this.context = this.canvas[0].getContext('2d');
+   this.context.fillStyle = color;
+   this.context.fillRect(0, 0, this.length, this.length);
+   this.context.globalCompositeOperation = 'destination-in';
+   this.context.drawImage(stamp_img,0,0);
+   return this.canvas[0];
+   /*
+   tmp.onload = function() {
+      self.length = tmp.width;
+      self.canvas = $('<canvas width="'+this.length+'px" height="'+this.length+'px">');
+      self.context = self.canvas[0].getContext('2d');
+      self.context.fillStyle = color;
+      self.context.fillRect(0, 0, self.length, self.length);
+   }
+   tmp.src = stamp_url;
+   
+   this.stamp = tmp; */
+
+/* create canvas of exact size 
+   this.canvas = $('<canvas width="'+this.length+'px" height="'+this.length+'px">');
+   this.context = this.canvas[0].getContext('2d');
+   this.context.fillStyle = color;
+   this.context.fillRect(0, 0, this.length, this.length);*/
+ //  this.context.globalCompositeOperation = 'destination-in';
+//this.context.drawImage(this.stamp,0,0);
+
+  // return this.canvas[0];
+    /*
     this.length = length || 30;
     this.color = color || 'black';
     this.canvas = $('<canvas width="'+length+'px" height="'+length+'px" style="border:1px solid gray;">');
     this.context = this.canvas[0].getContext('2d');
-    this.make();
+    this.make();*/
 }
 
 markerMaker.prototype.make = function () {
